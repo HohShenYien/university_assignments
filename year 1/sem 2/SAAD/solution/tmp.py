d3d562ef40906c16401d74acedfe86399f3ff0ee
HohShenYien <ysuper32310@gmail.com>
2021-06-24 00:17:41 +0800

    Added instructor page and sign attendance

--- year 1/sem 2/SAAD/solution/tmp.py
+++ year 1/sem 2/SAAD/solution/tmp.py
@@ -1,6 +1,26 @@
+import random
+
+num = 5
 with open("tmp.txt", "r") as file:
-    for line in file.readlines():
-        if "class_profile.html" in line:
-            print(line.replace("class_profile.html", "class_profile_2.html"), end="")
+    for name in file.readlines():
+        dice = random.randint(0,2)
+        if dice <= 1:
+            color = "bg-red"
+            checked = ""
+            active = ""
         else:
-            print(line, end="")
+            color = "bg-green"
+            checked = " checked"
+            active = "active"
+        print(
+            f"""
+                            <tr class="{color}">
+                                <td></td>
+                                <td><a href="member_profile_3.html" class="fs-20">{name.strip()}</a></td>
+                                <td>
+                                    <label for="defaultCheck{num}" class="{active}"></label>
+                                    <input class="form-check-input m-l-20" type="checkbox" value="" id="defaultCheck{num}"{checked}>
+                                </td>
+                            </tr>"""
+        )
+        num = num + 1
