2cdb6f4a68835d8b7a909686cd522bf3e3a15113
Hoh Shen Yien <55322546+HohShenYien@users.noreply.github.com>
2021-06-24 17:59:36 +0800

    Added everything about instructor and completed instructor UI

--- year 1/sem 2/SAAD/solution/tmp.py
+++ year 1/sem 2/SAAD/solution/tmp.py
@@ -3,24 +3,7 @@ import random
 num = 5
 with open("tmp.txt", "r") as file:
     for name in file.readlines():
-        dice = random.randint(0,2)
-        if dice <= 1:
-            color = "bg-red"
-            checked = ""
-            active = ""
-        else:
-            color = "bg-green"
-            checked = " checked"
-            active = "active"
-        print(
-            f"""
-                            <tr class="{color}">
-                                <td></td>
-                                <td><a href="member_profile_3.html" class="fs-20">{name.strip()}</a></td>
-                                <td>
-                                    <label for="defaultCheck{num}" class="{active}"></label>
-                                    <input class="form-check-input m-l-20" type="checkbox" value="" id="defaultCheck{num}"{checked}>
-                                </td>
-                            </tr>"""
-        )
-        num = num + 1
+        print(f"""<tr class="one-row">
+                        <td>{name.strip()}</td>
+                        <td><a href="member_profile_3.html"><i class="fa fa-info-circle"></i></a></td>
+                    </tr>""")
